52a39c1eab17ee78c85dce852e24e025875619eb
koba0909 <btyisu@gmail.com>
2023-03-25 01:44:30 +0900

    search모듈 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(project(":core:domain"))
     implementation(project(":core:data"))
     implementation(project(":core:data-implement"))
+    implementation(project(":feature:search"))
 
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
