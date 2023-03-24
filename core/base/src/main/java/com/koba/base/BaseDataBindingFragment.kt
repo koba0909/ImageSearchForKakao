9e00b7b9f4dc68b321dae07a0a10afdb6eeb920d
koba0909 <btyisu@gmail.com>
2023-03-25 02:06:57 +0900

    BaseDataBindingFragment 작성

---
+++ core/base/src/main/java/com/koba/base/BaseDataBindingFragment.kt
@@ -0,0 +1,36 @@
+package com.koba.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseDataBindingFragment<B : ViewDataBinding>(
+    @LayoutRes
+    private val layoutResId: Int,
+) : Fragment() {
+    protected var binding: B? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.lifecycleOwner = viewLifecycleOwner
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
