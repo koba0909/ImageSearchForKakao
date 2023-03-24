a6fd8a1434a8aa4a6d2958690855ea777f68416c
koba0909 <btyisu@gmail.com>
2023-03-25 02:05:41 +0900

    base 모듈 구성

---
+++ core/base/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+	alias(libs.plugins.android.library)
+	alias(libs.plugins.kotlin.android)
+}
+
+android {
+	namespace = "com.koba.base"
+	compileSdk = libs.versions.compileSdk.get().toInt()
+
+	buildFeatures {
+		dataBinding = true
+	}
+
+	defaultConfig {
+		minSdk = libs.versions.minSdk.get().toInt()
+		targetSdk = libs.versions.targetSdk.get().toInt()
+	}
+
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_1_8
+		targetCompatibility = JavaVersion.VERSION_1_8
+	}
+}
+
+dependencies {
+	implementation(libs.androidx.appcompat)
+	implementation(libs.kotlinx.coroutines.android)
+	implementation(libs.androidx.core.ktx)
+}
