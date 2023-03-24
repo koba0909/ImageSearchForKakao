f98ffa714f8f36bd58f8690c40c7333cf29c8dd8
koba0909 <btyisu@gmail.com>
2023-03-25 02:34:18 +0900

    SearchFragment 추가

--- feature/search/build.gradle.kts
+++ feature/search/build.gradle.kts
@@ -8,6 +8,10 @@ android {
 	namespace = "com.koba.search"
 	compileSdk = libs.versions.compileSdk.get().toInt()
 
+	buildFeatures {
+		dataBinding = true
+	}
+
 	defaultConfig {
 		minSdk = libs.versions.minSdk.get().toInt()
 		targetSdk = libs.versions.targetSdk.get().toInt()
@@ -20,9 +24,12 @@ android {
 }
 
 dependencies {
+	implementation(project(":core:base"))
 	implementation(project(":core:domain"))
 
+	implementation(libs.androidx.constraintlayout)
 	implementation(libs.kotlinx.coroutines.android)
+	implementation(libs.androidx.appcompat)
 	implementation(libs.hilt)
 	kapt(libs.hilt.compiler)
 }
---
+++ feature/search/build/generated/source/kapt/debug/com/koba/search/BR.java
@@ -0,0 +1,5 @@
+package com.koba.search;
+
+public class BR {
+  public static int _all = 0;
+}
