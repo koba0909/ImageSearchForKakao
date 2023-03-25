d8391cb5cb726cf74f2247674b331bb0a684a3f5
koba0909 <btyisu@gmail.com>
2023-03-25 16:02:01 +0900

    SearchReusltViewHolder 패키지 이동

--- feature/search/src/main/java/com/koba/search/SearchListAdapter.kt
+++ feature/search/src/main/java/com/koba/search/SearchListAdapter.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.koba.domain.model.ImageModel
+import com.koba.search.viewholder.SearchResultViewHolder
 
 class SearchListAdapter : ListAdapter<ImageModel, RecyclerView.ViewHolder>(diffCallback) {
 
