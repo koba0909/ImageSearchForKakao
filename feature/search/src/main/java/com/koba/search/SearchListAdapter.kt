dead2e7c5c63b631089c1e50ef6104f3dd56f068
koba0909 <btyisu@gmail.com>
2023-03-25 16:01:27 +0900

    SearchListAdatper 및 SearchResultViewHolder 구성

---
+++ feature/search/src/main/java/com/koba/search/SearchListAdapter.kt
@@ -0,0 +1,44 @@
+package com.koba.search
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.koba.domain.model.ImageModel
+
+class SearchListAdapter : ListAdapter<ImageModel, RecyclerView.ViewHolder>(diffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return SearchResultViewHolder(
+            binding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.holder_search_result,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is SearchResultViewHolder -> holder.bind(getItem(position))
+        }
+    }
+
+
+    companion object {
+        private val diffCallback = object : DiffUtil.ItemCallback<ImageModel>() {
+            override fun areItemsTheSame(
+                oldItem: ImageModel,
+                newItem: ImageModel,
+            ) = oldItem.url == newItem.url
+
+            override fun areContentsTheSame(
+                oldItem: ImageModel,
+                newItem: ImageModel,
+            ) = oldItem == newItem
+        }
+    }
+}
