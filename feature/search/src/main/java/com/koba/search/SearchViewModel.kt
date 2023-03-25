59472427a4fa99f0c655bed9480558b582f81c9c
koba0909 <btyisu@gmail.com>
2023-03-25 21:37:10 +0900

    검색 키워드 debounce 처리

--- feature/search/src/main/java/com/koba/search/SearchViewModel.kt
+++ feature/search/src/main/java/com/koba/search/SearchViewModel.kt
@@ -7,6 +7,8 @@ import com.koba.domain.model.SearchResult
 import com.koba.domain.usecase.GetImageSearchResultListUseCase
 import com.koba.domain.usecase.GetVideoSearchResultListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -25,43 +27,60 @@ class SearchViewModel @Inject constructor(
     private val getVideoSearchResultListUseCase: GetVideoSearchResultListUseCase,
 ) : ViewModel() {
     private val _isLoading = MutableStateFlow(false)
-    private val _imageList = MutableStateFlow<List<SearchResult>>(emptyList())
 
-    val uiState = combine(_isLoading, _imageList) { isLoading, imageList ->
+    private val _keyword = MutableSharedFlow<String>()
+
+    @OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
+    private val _searchResult = _keyword.debounce(500)
+        .flatMapLatest { getSearchResultFlow(it) }
+        .onStart {
+            _isLoading.update { true }
+        }
+        .onCompletion {
+            _isLoading.update { false }
+        }
+
+    val uiState = combine(_isLoading, _searchResult) { isLoading, searchResult ->
         when {
             isLoading -> SearchUIState.Loading
-            imageList.isEmpty() -> SearchUIState.Empty
-            else -> SearchUIState.Show(imageList)
+            searchResult.isEmpty() -> SearchUIState.Empty
+            else -> SearchUIState.Show(searchResult)
         }
-    }
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = SearchUIState.Empty,
+    )
 
     fun onSearchKeyword(keyword: String): Boolean {
-        _imageList.update {
-            emptyList()
-        }
-
         viewModelScope.launch {
-            combine(
-                getImageSearchResultListUseCase(keyword),
-                getVideoSearchResultListUseCase(keyword),
-            ) { images, videos ->
-                images + videos
-            }.map { searchResults ->
-                searchResults.sortedBy { it.dateTime }
-            }.onStart {
-                _isLoading.update { true }
-            }.onCompletion {
-                _isLoading.update { false }
-            }.catch { e ->
-                Log.d("hugh", e.message ?: "")
-            }.collect {
-                _imageList.update { it }
-            }
+            _keyword.emit(keyword)
         }
         return false
     }
 
+    private suspend fun getSearchResultFlow(keyword: String): Flow<List<SearchResult>> {
+        if (keyword.isBlank()) {
+            return flowOf(emptyList())
+        }
+
+        return combine(
+            getImageSearchResultListUseCase(keyword),
+            getVideoSearchResultListUseCase(keyword),
+        ) { images, videos ->
+            images + videos
+        }.map { searchResults ->
+            searchResults.sortedBy { it.dateTime }
+        }.catch {
+            it.message?.let { Log.e(TAG, it) }
+        }
+    }
+
     fun onPickImage(searchResult: SearchResult) {
         // TODO image 선택 후 보관함에 저장
+    }
+
+    companion object {
+        private const val TAG = "SearchViewModel"
     }
 }
