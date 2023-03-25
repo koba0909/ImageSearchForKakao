d1186106d3c09e1e5c6c2b9e8d452906b02ff8df
koba0909 <btyisu@gmail.com>
2023-03-25 14:51:52 +0900

    binding nullable 하지 않게 사용하도록 처리

--- feature/search/src/main/java/com/koba/search/SearchViewModel.kt
+++ feature/search/src/main/java/com/koba/search/SearchViewModel.kt
@@ -1,8 +1,14 @@
 package com.koba.search
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class SearchViewModel : ViewModel(){
+class SearchViewModel @Inject constructor() : ViewModel() {
+    fun onSearchKeyword(keyword: String): Boolean {
+        Log.d("hugh", keyword)
+        return false
+    }
 }
