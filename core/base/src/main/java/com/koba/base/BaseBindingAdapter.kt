ccecbd7bec88ae0b52a30bd70c8770275b5aec93
koba0909 <btyisu@gmail.com>
2023-03-25 15:44:55 +0900

    glide를 사용하여 url 이미지 노출

--- core/base/src/main/java/com/koba/base/BaseBindingAdapter.kt
+++ core/base/src/main/java/com/koba/base/BaseBindingAdapter.kt
@@ -1,10 +1,19 @@
 package com.koba.base
 
 import android.view.View
+import android.widget.ImageView
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 
 @BindingAdapter("isVisible")
 fun View.isVisible(isVisible: Boolean) {
     this.isVisible = isVisible
+}
+
+@BindingAdapter("bindImageUrl")
+fun ImageView.bindImageUrl(url: String) {
+    Glide.with(context)
+        .load(url)
+        .into(this)
 }
