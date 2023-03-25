31f478a82c6da682d9dd7ff70b120b4b6f3e6a60
koba0909 <btyisu@gmail.com>
2023-03-25 15:26:43 +0900

    SearchUiState별 뷰 visible 구성

--- feature/search/build.gradle.kts
+++ feature/search/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
 	implementation(project(":core:base"))
 	implementation(project(":core:domain"))
 
+	implementation(libs.androidx.recyclerview)
 	implementation(libs.androidx.constraintlayout)
 	implementation(libs.kotlinx.coroutines.android)
 	implementation(libs.androidx.appcompat)
