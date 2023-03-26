937f1f56b5d2f3c8ae4934720f6cfe9fdaf9dac0
koba0909 <btyisu@gmail.com>
2023-03-26 15:11:15 +0900

    ViewModel init시 저장된 이미지 불러오고 이미지 삭제, 저장시 리스트 새로 불러오도록 수정

--- feature/storage/src/main/java/com/koba/storage/StorageViewModel.kt
+++ feature/storage/src/main/java/com/koba/storage/StorageViewModel.kt
@@ -1,11 +1,15 @@
 package com.koba.storage
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.koba.domain.model.SavedImage
 import com.koba.domain.usecase.DeleteImageUrlUseCase
 import com.koba.domain.usecase.GetSavedImageListUseCase
 import com.koba.domain.usecase.SaveImageUrlUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 sealed interface StorageUiState {
@@ -20,4 +24,57 @@ class StorageViewModel @Inject constructor(
     private val saveImageUrlUseCase: SaveImageUrlUseCase,
     private val deleteImageUrlUseCase: DeleteImageUrlUseCase,
 ) : ViewModel() {
+    private val _savedImageList = MutableStateFlow<List<SavedImage>>(emptyList())
+
+    val uiState = _savedImageList.map {
+        if (it.isEmpty()) {
+            StorageUiState.Empty
+        } else {
+            StorageUiState.HaveList(it)
+        }
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = StorageUiState.Empty,
+    )
+
+    init {
+        fetchSavedImages()
+    }
+
+    private fun fetchSavedImages() {
+        viewModelScope.launch {
+            runCatching {
+                getSavedImageListUseCase()
+            }.onSuccess {
+                _savedImageList.update { list ->
+                    list.sortedBy { it.savedTime }
+                }
+            }.onFailure { e ->
+                e.message?.let { Log.e(TAG, it) }
+            }
+        }
+    }
+
+    fun saveImage(thumbnailUrl: String) {
+        viewModelScope.launch {
+            saveImageUrlUseCase(
+                thumbnailUrl,
+                System.nanoTime(),
+            )
+
+            fetchSavedImages()
+        }
+    }
+
+    fun deleteImage(thumbnailUrl: String) {
+        viewModelScope.launch {
+            deleteImageUrlUseCase(thumbnailUrl)
+            fetchSavedImages()
+        }
+    }
+
+    companion object {
+        const val TAG = "StorageViewModel"
+    }
 }
