e37ffbdc432aa39ef33216ea22a281920dd1dff7
koba0909 <btyisu@gmail.com>
2023-03-26 15:21:44 +0900

    adapter세팅

--- feature/storage/src/main/java/com/koba/storage/StorageFragment.kt
+++ feature/storage/src/main/java/com/koba/storage/StorageFragment.kt
@@ -9,9 +9,25 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class StorageFragment : BaseDataBindingFragment<FragmentStorageBinding>(R.layout.fragment_storage) {
-    val storageViewModel: StorageViewModel by viewModels()
+
+    private val storageViewModel: StorageViewModel by viewModels()
+
+    private val storageListAdapter: StorageListAdapter by lazy {
+        StorageListAdapter(
+            onPickImage = {
+                storageViewModel.deleteImage(it.thumbnailUrl)
+            },
+        )
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        with(binding) {
+			recyclerViewStorage.adapter = storageListAdapter
+        }
     }
 }
