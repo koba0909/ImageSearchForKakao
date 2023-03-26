4ee0410070f2b70f0261f1b1bd2052f88b0faba0
koba0909 <btyisu@gmail.com>
2023-03-26 14:54:28 +0900

    StorageUiState 정의

--- feature/storage/src/main/java/com/koba/storage/StorageFragment.kt
+++ feature/storage/src/main/java/com/koba/storage/StorageFragment.kt
@@ -1,5 +1,7 @@
 package com.koba.storage
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
 import com.koba.base.BaseDataBindingFragment
 import com.koba.storage.databinding.FragmentStorageBinding
@@ -7,5 +9,9 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class StorageFragment : BaseDataBindingFragment<FragmentStorageBinding>(R.layout.fragment_storage) {
-	val storageViewModel: StorageViewModel by viewModels()
+    val storageViewModel: StorageViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
--- feature/storage/src/main/java/com/koba/storage/StorageViewModel.kt
+++ feature/storage/src/main/java/com/koba/storage/StorageViewModel.kt
@@ -1,7 +1,23 @@
 package com.koba.storage
 
 import androidx.lifecycle.ViewModel
+import com.koba.domain.model.SavedImage
+import com.koba.domain.usecase.DeleteImageUrlUseCase
+import com.koba.domain.usecase.GetSavedImageListUseCase
+import com.koba.domain.usecase.SaveImageUrlUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+sealed interface StorageUiState {
+    object Empty : StorageUiState
+
+    data class HaveList(val savedImageList: List<SavedImage>) : StorageUiState
+}
 
 @HiltViewModel
-class StorageViewModel : ViewModel()
+class StorageViewModel @Inject constructor(
+    private val getSavedImageListUseCase: GetSavedImageListUseCase,
+    private val saveImageUrlUseCase: SaveImageUrlUseCase,
+    private val deleteImageUrlUseCase: DeleteImageUrlUseCase,
+) : ViewModel() {
+}
